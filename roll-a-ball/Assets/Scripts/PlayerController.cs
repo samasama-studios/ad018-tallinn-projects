a89d06ff5b9c901c43c75facd161bbc74f090ada
Pepi <jose@samasama.studio>
2019-03-26 11:15:10 +0200

    Add roll-a-ball project core.

---
+++ roll-a-ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    private Rigidbody rigidbody;
+
+    private void Start () {
+        rigidbody = GetComponent<Rigidbody> ();
+    }
+
+    private void FixedUpdate () {
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveVertical = Input.GetAxis ("Vertical");
+
+        Vector3 movement = new Vector3 (moveHorizontal, 0f, moveVertical);
+        rigidbody.AddForce (speed * movement);
+    }
+
+    private void OnTriggerEnter (Collider other) {
+        if (other.gameObject.CompareTag ("Pick Up")) {
+            other.gameObject.SetActive (false);
+        }
+
+    }
+
+}
---
+++ roll-a-ball/Assets/Scripts/Rotator.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+
+    private void Update () {
+        transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime);
+    }
+
+}
