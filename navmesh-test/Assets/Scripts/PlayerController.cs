ccf915baff35c36032dcdd06a6635b606c46a44a
Pepi <jose@samasama.studio>
2019-03-26 11:50:17 +0200

    Add NavMesh test project.

---
+++ navmesh-test/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour {
+
+    public Camera camera;
+    public NavMeshAgent agent;
+
+    private void Update () {
+        if (Input.GetMouseButtonDown (0)) {
+            Ray ray = camera.ScreenPointToRay (Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast (ray, out hit)) {
+                agent.SetDestination (hit.point);
+            }
+        }
+    }
+
+}
